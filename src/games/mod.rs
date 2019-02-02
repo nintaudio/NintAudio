484ef4ddcfa4747fe58aed1ed2c95a83ba8998b7
JeremiePlamondon <jeremieplam@protonmail.com>
2019-02-02 17:15:54 -0500

    Created Mole test game

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -1,6 +1,7 @@
 use clap::{clap_app, crate_authors, crate_description, crate_version};
 
 mod demo;
+mod mole_game;
 
 pub trait Game {
     fn update(&mut self, act: Option<Action>, device: &rodio::Device) -> bool;
@@ -17,6 +18,7 @@ pub enum Action {
 pub fn select(device: &rodio::Device) -> Box<dyn Game> {
     let subcommands = vec![
         clap_app!(demo => (long_about: demo::description())(about: demo::about())),
+        clap_app!(mole => (long_about: mole_game::description())(about: mole_game::about())),
     ];
     let mat = clap_app!(nintaudio =>
       (version: crate_version!())
@@ -28,6 +30,7 @@ pub fn select(device: &rodio::Device) -> Box<dyn Game> {
 
     match mat.subcommand_name() {
         Some("demo") => Box::new(demo::new(device)),
+        Some("mole") => Box::new(mole_game::new(device)),
         None => panic!("Please provide a required subcommand"),
         _ => unreachable!(),
     }
