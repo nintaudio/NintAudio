17c42cec961fc81016b384a227246b6ffbdd9842
JeremiePlamondon <jeremieplam@protonmail.com>
2019-02-03 08:24:01 -0500

    Almost finished Moles

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -10,7 +10,7 @@ struct Assets;
 
 mod breakout;
 mod demo; // ça
-//mod moles;
+mod moles;
 mod pong;
 
 pub fn audio(file: &str) -> Decoder<Cursor<std::borrow::Cow<'_, [u8]>>> {
@@ -49,7 +49,7 @@ pub fn select(device: &rodio::Device) -> Box<dyn Game> {
         clap_app!(demo => (long_about: demo::description())(about: demo::about())), // ça
         clap_app!(breakout => (long_about: breakout::description())(about: breakout::about())),
         clap_app!(pong => (long_about: pong::description())(about: pong::about())),
-        //clap_app!(moles => (long_about: moles::description())(about: moles::about())),
+        clap_app!(moles => (long_about: moles::description())(about: moles::about())),
     ];
     let clap = clap_app!(nintaudio =>
       (version: crate_version!())
@@ -61,7 +61,7 @@ pub fn select(device: &rodio::Device) -> Box<dyn Game> {
     match clap.get_matches().subcommand_name() {
         Some("demo") => Box::new(demo::new(device)), // ça
         Some("pong") => Box::new(pong::new(device)),
-        //Some("moles") => Box::new(moles::new(device)),
+        Some("moles") => Box::new(moles::new(device)),
         Some("breakout") => Box::new(breakout::new(device)),
         None => {
             println!("Please provide a required game name as first argument");
--- src/games/moles.rs
+++ src/games/moles.rs
@@ -27,9 +27,9 @@ pub fn new(device: &rodio::Device) -> Moles {
         moles: [false; 3],
         unspawn: 3,
         spawn: 0,
-        spawn_time: 2_00, 
-        spawn_rate: 1_00, 
-        game_time: 60_00}
+        spawn_time: 3_00/2, 
+        spawn_rate: 3_00/2, 
+        game_time: 60_00/2}
 }
 
 impl Game for Moles {
@@ -37,20 +37,23 @@ impl Game for Moles {
     
     //Game Timer
     self.game_time -= 1;
-    
+    if self.game_time == 0{
+        return Some(self.score as u32);
+    }
+
     //Spawn Timer
-    self.spawn_time = if self.spawn_time == (self.spawn_rate/2){
+    self.spawn_time = if self.spawn_time == 20{
             //Spawn New Mole
-            self.spawn = spawn(self.unspawn, self.spawn, &mut self.moles, &device);
+            self.spawn = spawn(self.unspawn, &mut self.moles, &device);
             
 
             //Reduce Timer
             self.spawn_rate -= if self.spawn_rate > 25{
-                    1
+                    2
                 }else{
                     0
                 };
-            (self.spawn_rate/2) -1 
+            19
         }else if self.spawn_time == 0{
             //Unspawn Old Mole
             unspawn(self.unspawn, self.spawn, &mut self.moles, &device);
@@ -63,13 +66,13 @@ impl Game for Moles {
 
     match act {
       Some(Action::Left) => {
-            self.score += action_check(self.moles[0]);
+            self.score += action_check(self.moles[0], 0, &device);
         },
       Some(Action::Right) => {
-            self.score += action_check(self.moles[2]);
+            self.score += action_check(self.moles[2], 2, &device);
         },
       Some(Action::Up) => {
-            self.score += action_check(self.moles[1]);
+            self.score += action_check(self.moles[1], 1, &device);
         },
       _ => {},
     };
@@ -83,7 +86,7 @@ impl Game for Moles {
              self.left_count, 
              self.right_count, 
              self.score, 
-             self.game_time/100, 
+             self.game_time/50, 
              self.spawn_time,
              if self.moles[0]{"1"}else{"0"},
              if self.moles[1]{"1"}else{"0"},
@@ -97,26 +100,30 @@ impl Game for Moles {
 fn unspawn(unspawn: u8, spawn: u8, moles: &mut [bool; 3], device: &rodio::Device){
     if unspawn < 3{
         moles[unspawn as usize] = false;
+        once(device, "enemy_unspawn.ogg", x(unspawn), y(unspawn));
     }
-    once(device, "enemy_unspawn.mp3", x(unspawn), y(unspawn));
 }
 
-fn spawn(unspawn: u8, spawn: u8, moles: &mut [bool; 3], device: &rodio::Device) -> u8{
+fn spawn(unspawn: u8, moles: &mut [bool; 3], device: &rodio::Device) -> u8{
+    let spawn: u8;
     loop{
         let slot = rand::thread_rng().gen_range(0,3) as usize;
         if !moles[slot] && (slot as u8) != unspawn{
             moles[slot] = true;
-            once(device, "enemy_spawn.mp3", x(spawn), y(spawn));
+            spawn = slot as u8;
+            once(device, "enemy_spawn.ogg", x(spawn), y(spawn));
             break;
         }
     }
     spawn
 }
 
-fn action_check(hit: bool) -> i16{
+fn action_check(hit: bool, emitter: u8, device: &rodio::Device) -> i16{
     if hit{
+        once(device, "swing_hit.ogg", x(emitter), y(emitter));
         2
     }else{
+        once(device, "swing_miss_hit.ogg", x(emitter), y(emitter));
         -1
     }
 }
