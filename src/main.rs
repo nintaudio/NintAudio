9f342919cd153915d70989d1c7c95b13d8baa70c
Xavier L'Heureux <xavier.lheureux@icloud.com>
2019-02-04 09:20:39 -0500

    Add failing tests on errors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![cfg_attr(test, deny(bad_style,
+       const_err,
+       dead_code,
+       improper_ctypes,
+       legacy_directory_ownership,
+       non_shorthand_field_patterns,
+       no_mangle_generic_items,
+       overflowing_literals,
+       path_statements ,
+       patterns_in_fns_without_body,
+       plugin_as_library,
+       private_in_public,
+       safe_extern_statics,
+       unconditional_recursion,
+       unions_with_drop_fields,
+       unused,
+       unused_allocation,
+       unused_comparisons,
+       unused_parens,
+       while_true))]
+
+// Allowed by default
+#![cfg_attr(test, deny(missing_docs,
+       trivial_numeric_casts,
+       unused_extern_crates,
+       unused_import_braces,
+       unused_qualifications))]
+
 use std::io::{stdout, Write};
 use std::sync::mpsc;
 use std::thread;
